de3142d059e61be39d72e5c0cc8620c2afe2e7c5
git12nazneen <lipinazneen65@gmail.com>
2024-03-28 02:58:26 +0600

    link

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -7,24 +7,25 @@ const Book = ({book}) => {
    
     const {id,bookName, author, image, rating, category, tags} = book
     return (
-        <div>
+        <Link to={`/book/${id}`}>
          
             <div className="flex flex-col w-80 p-5 my-4 space-y-6 overflow-hidden rounded-lg shadow-md border border-gray-300 text-black">
-	<div>
-		<Link to={`/book/${id}`}>
-        <img src={image} alt="" className=" w-full mb-4 lg:h-52 sm:h-96 bg-gray-500" />
-        </Link>
-        <div className="flex flex-wrap justify-between my-3">
-            <div className="space-x-2 text-green-600 bg-green-100 rounded-2xl px-4 py-2 text-sm">
-                {tags[0]}
+
+	    <div>
+            
+            <img src={image} alt="" className=" w-full mb-4 lg:h-52 sm:h-96 bg-gray-500" />
+           
+            <div className="flex flex-wrap justify-between my-3">
+                <div className="space-x-2 text-green-600 bg-green-100 rounded-2xl px-4 py-2 text-sm">
+                    {tags[0]}
+                </div>
+                <div className="space-x-2 text-green-600 bg-green-100 rounded-2xl px-4 py-2 text-sm">
+                {tags[1]}
+                </div>
             </div>
-            <div className="space-x-2 text-green-600 bg-green-100 rounded-2xl px-4 py-2 text-sm">
-            {tags[1]}
-            </div>
-        </div>
-		<h2 className="mb-1 text-xl font-bold text-gray-700">{bookName}</h2>
-		<p className="text-sm text-gray-600 mb-4">By {author}</p>
-        <hr />
+            <h2 className="mb-1 text-xl font-bold text-gray-700">{bookName}</h2>
+            <p className="text-sm text-gray-600 mb-4">By {author}</p>
+            <hr />
 	</div>
 	<div className="flex flex-wrap justify-between">
 		<div className="space-x-2 text-gray-600">
@@ -36,8 +37,9 @@ const Book = ({book}) => {
             
 		</div>
 	</div>
-</div>
-        </div>
+            </div>
+     
+         </Link>
     );
 };
 
